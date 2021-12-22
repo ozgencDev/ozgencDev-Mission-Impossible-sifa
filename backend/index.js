79b3fdab6a7cfbe912ba13afed9fbbc98f7f512d
furkan ceylan <ceylan.furkan@outlook.com>
2021-12-22 20:11:31 +0300

    Edit db connection

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,10 @@ const mysql = require('mysql');
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const cors = require('cors')
-const {createPool} = require("mysql");
 
 const app = express();
 
-// const usersRoute = require('./routes/users')
-
-const dbConfig = createPool({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  database: "usersdb",
-  password: "new123",
-  connectionLimit: 10,
-})
-
+const usersRoute = require('./routes/user.routes.js')
 
 //cors options
 const corsOptions = {
@@ -25,21 +14,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "new123",
-  database: "usersdb",
-});
-
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
 
 dotenv.config();
 app.use(express.json());
@@ -57,16 +31,14 @@ app.get('/createdb', (req, res) => {
     });
 });
 
-
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to mission impossible application." });
 });
 
-
 require("./routes/user.routes.js")(app);
 
-// app.use('/api/users', usersRoute)
+app.use('/api/users', usersRoute)
 
 const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
